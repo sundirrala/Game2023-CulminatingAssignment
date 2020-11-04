2e10defad605dae1d3d1a73ed4a4a91e37a707ff
sundirrala <nisara.drahman@gmail.com>
2020-11-03 20:54:52 -0500

    player teleporting movement

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    // ------------- serializedfield makes private variable seen on the inspector ----------------- //
+    [SerializeField]
+    float playerSpeed = 5.0f;
+
+    // ------ the update function is called once every frame ----------- //
+    void Update()
+    {
+        // ------------ teleporting the player, no animations yet. ------------- //
+        Vector2 movementVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementVec *= Time.deltaTime * playerSpeed;
+        transform.Translate(movementVec);
+        // --------------- end of teleporting player code. ---------------- //
+    }
+}
