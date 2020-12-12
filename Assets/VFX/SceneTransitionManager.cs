252a08edd098784bf4f11010fd0b0ec7b3d901a2
Ramin <zeuspsn.55555@gmail.com>
2020-12-12 01:20:15 -0500

    final changes

---
+++ Assets/VFX/SceneTransitionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    [SerializeField]
+    Animator screenEffects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var encounterMgr = SpawnPoint.player.GetComponent<EncounterManager>();
+        encounterMgr.onEnterEncounter.AddListener(OnEnterEncounterHandler);
+        encounterMgr.onExitEncounter.AddListener(OnExitEncounterHandler);
+    }
+
+    void OnEnterEncounterHandler()
+    {
+        screenEffects.Play("FadeToBlackState");
+        //screenEffects.Play("FadeFromBlackState");
+    }
+
+    void OnExitEncounterHandler()
+    {
+        screenEffects.Play("FadeToBlackState");
+    }
+}
